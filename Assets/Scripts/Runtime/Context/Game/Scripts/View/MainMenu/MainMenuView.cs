44e591a6e54f2f6fd90f3af1e73f3250a515f7df
Maybe One Day <silmen@jib.gs>
2023-08-01 20:03:58 +0300

    update

--- Assets/Scripts/Runtime/Context/Game/Scripts/View/MainMenu/MainMenuView.cs
+++ Assets/Scripts/Runtime/Context/Game/Scripts/View/MainMenu/MainMenuView.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using strange.extensions.mediation.impl;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Runtime.Context.Game.Scripts.View.MainMenu
 {
@@ -9,11 +12,106 @@ namespace Runtime.Context.Game.Scripts.View.MainMenu
   {
     public TextMeshProUGUI enemyHealthLabel;
 
+    public Transform playerContainer;
+
+    private int _counter = 0;
+
     public void UpdateEnemyHealth(float enemyHealth)
     {
-      // enemyHealthLabel.text = enemyHealth.ToString();
+      enemyHealthLabel.text = enemyHealth.ToString();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void LateUpdate()
+    {
+    }
+
+    private int _counter2 = 0;
+    public void TestMethod()
+    {
+      while (true)
+      {
+        Debug.Log("TestMethod");
+
+        _counter2++;
+        
+        if (_counter2 == 1000000000)
+        {
+          break;
+        }
+      }
     }
     
+    public IEnumerator TestMethod2()
+    {
+      while (true)
+      {
+        Debug.Log("TestMethod");
+
+        _counter2++;
+        
+        if (_counter2 == 1000000000)
+        {
+          break;
+        }
+
+        yield return new WaitForEndOfFrame();
+        
+        // yield break;
+        
+        Debug.Log("TestMethod");
+      }
+    }
+    
+    public IEnumerator ShowAndHideMessage()
+    {
+      // Show();
+
+      yield return new WaitForSeconds(2f);
+      
+      // Hide();
+    }
+
+    private IEnumerator CoroutineMethod()
+    {
+      while (true)
+      {
+        Debug.Log("TestMethod - 0");
+
+        yield return new WaitForSeconds(2f);
+        
+        Debug.Log("TestMethod - 1");
+      }
+    }
+
+    public void OnCoroutineExampleClick()
+    {
+      StartCoroutine(CoroutineMethod());
+      //
+      //
+      //
+      //
+      //
+    }
+
+    public void OnCreateObjectClick()
+    {
+      AsyncOperationHandle<GameObject> asyncOperation = Addressables.InstantiateAsync("PlayerObject", playerContainer);
+      asyncOperation.Completed += OnCreateObjectCompleted;
+    }
+
+    private void OnCreateObjectCompleted(AsyncOperationHandle<GameObject> obj)
+    {
+      obj.Result.name = $"PlayerObject_{_counter++}";
+    }
+
     // protected override void Awake()
     // {
     //   base.Awake();
